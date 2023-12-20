5b3508c2fc00ebd8bcb4b6b6143cf52b5ad65f82
Harald Schneider <h_schneider@marketmix.com>
2023-12-20 09:10:15 +0100

    Update main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -17,23 +17,12 @@ Neutralino.events.on("windowClose", onWindowClose);
 // Check for update
 //
 let opt = {
-    lang: 'en',
+    lang: 'de',
     debug: true,
     arch: 'x64',
     token: 'hB9rV7cS3tD3bU1wA8vY3pQ5fO4qO6sP'
 }
 let AUTOUPDATE = new NeutralinoAutoupdate("https://autoupdate.test/demo/manifest.php", opt);
-
-let langFR = {
-    'txtNewVersion': 'Une nouvelle version de {appName} est disponible.',
-    'txtAskUpdate': 'Vous disposez de la version {versionCurrent}. Voulez-vous installer la version {versionUpdate}?',
-    'btnCancel': 'Pas maintenant',
-    'btnOK': 'À installer',
-    'errorChecksum': "Ooops - Erreur de mise à jour: le téléchargement semble être interrompu.<br>Vous pouvez fermer cette boîte de dialogue et réessayer plus tard.",
-    'errorUnpack': 'Ooops - Erreur de mise à jour: le téléchargement ne peut pas être décompressé.<br>Vous pouvez fermer cette boîte de dialogue et réessayer plus tard.'
-}
-AUTOUPDATE.langStrings['fr'] = langFR;
-AUTOUPDATE.lang = 'fr';
 
 (async () => {
     await AUTOUPDATE.check();
